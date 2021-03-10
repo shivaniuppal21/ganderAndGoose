4bc9a440045cf870e27808936fd4c5c4ceb4c684
shivani <shivaniuppal21@gmail.com>
2021-03-10 17:45:54 -0500

    adding products

--- backend/db/schema/seed_tofix.sql
+++ backend/db/schema/seed_tofix.sql
@@ -11,12 +11,76 @@ CREATE TABLE IF NOT EXISTS Users (
   country varchar(127),
   isAdmin boolean
 );
-CREATE TABLE Products (
+CREATE TABLE IF NOT EXISTS Products (
   product_id varchar(127) PRIMARY KEY not null,
   title varchar(255) not null,
   description text not null,
   created_on TIMESTAMP NOT NULL,
   price VARCHAR,
-  reviews SMALLINT(1),
+  reviews INT,
   category VARCHAR
 );
+Insert INTO
+  Users (
+    ID,
+    first_name,
+    last_name,
+    email,
+    password,
+    mobile,
+    zipCode,
+    country,
+    isAdmin
+  )
+VALUES
+  (
+    '1',
+    'uday',
+    'kakkar',
+    'udaykakkar',
+    'uday@123',
+    '12345',
+    '23445657',
+    'canada',
+    'false'
+  ),
+  (
+    '2',
+    'john',
+    'fernadis',
+    'johnfernadis',
+    'john@123',
+    '98765',
+    '98445657',
+    'canada',
+    'false'
+  );
+Insert INTO
+  Products (
+    product_id,
+    title,
+    description,
+    created_on,
+    price,
+    reviews,
+    category
+  )
+VALUES
+  (
+    '1',
+    'name signs',
+    'Name Sign - Wooden Name Sign - Name - Personalized Wall Sign',
+    '2021-01-19 03:14:07',
+    '85',
+    3,
+    'name planque'
+  ),
+  (
+    '2',
+    'growth chart',
+    'personalized wooden growth chart wooden growth ruler wood growth chart personalized name wooden numbers engraved lines',
+    '2021-01-19 03:14:07',
+    '100',
+    4,
+    'toys'
+  );
