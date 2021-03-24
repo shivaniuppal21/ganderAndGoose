2f3ca1748688444e5ac8bfb829def102fab02e41
Megha <meghagupta0891@gmail.com>
2021-03-24 03:07:24 -0400

    Added total with tax calc in code

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -1,8 +1,13 @@
 import React from "react";
 import CartItem from "./cartItem";
+import './cart.css';
 
 export default function Cart(props) {
   const cart = props.cart;
+  let total = 0 ; 
+  Object.keys(cart).map(item=>{
+    total += cart[item].product.price * cart[item].amount;
+  })
   const cartKeys = Object.keys(cart || {});
   return (
     <>
@@ -23,6 +28,22 @@ export default function Cart(props) {
                 removeFromCart={props.removeFromCart}
               />
             ))}
+            <div id="displayTotal" className="column is-12 is-clearfix">
+              <br/>
+              <div>
+                <div className="column box" style={{textAlign:"center"}}>
+                <span>Item SubTotal:&nbsp;&nbsp;&nbsp; </span><span>${total}</span>
+                <br/>
+                Shipping:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FREE
+                <br/>
+                HST:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${Math.ceil(0.13 * total,2)}
+                <hr/>
+                <div style={{fontWeight: '700',fontSize: '1.25rem'}}>
+                Total: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CAD ${total + Math.ceil(0.13 * total,2)}
+                </div>
+                </div>
+              </div>
+            </div>
             <div className="column is-12 is-clearfix">
               <br />
               <div className="is-pulled-right">
